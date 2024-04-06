8c85bd1434594259c4d4c1889085656cf27217ae
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-04-07 02:17:41 +0530

    complete starter template

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
+import { ThemeProvider } from "./providers/theme-providers.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
