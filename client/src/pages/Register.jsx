e77771f03aa1594d64530322126387f722e6c314
shubhampriya1 <shubhamp.ug21.ee@nitp.ac.in>
2024-04-15 19:36:32 +0530

    made register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,59 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
 function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <Card className="w-[350px]  md:w-96 m-auto my-16">
+      <CardHeader>
+        <CardTitle>Create Account</CardTitle>
+        <CardDescription>
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-sky-400">
+            Login
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col items-start gap-1 space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Your name" />
+            </div>
+            <div className="flex flex-col items-start gap-1 space-y-1.5">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" placeholder="Username" />
+            </div>
+            <div className="flex flex-col items-start gap-1 space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" placeholder="johndoe@example.com" />
+            </div>
+            <div className="flex flex-col items-start gap-1 space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" placeholder="Enter your password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link to={"/"}>
+          <Button variant="outline">Cancel</Button>
+        </Link>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default RegisterPage;
