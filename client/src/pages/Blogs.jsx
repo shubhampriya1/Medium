f5abd6f830939062d79b609900536cb1024db803
Shubham priya <141516634+shubhampriya1@users.noreply.github.com>
2024-08-03 16:47:34 +0530

    Update Blogs.jsx

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,34 +1,47 @@
 import Card from "@/components/card";
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import axios from "axios";
-import { Loader2 } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import { Loader2, TriangleAlert } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function BlogPage() {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalBlogs, setTotalBlogs] = useState(0);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    async function getBlogs() {
-      try {
-        setLoading(true);
-        const { data } = await axios.get(
-          `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/blog/getBlogs`
-        );
-
-        setBlogs(data);
-
-        console.log(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
+  async function fetchBlog({ page = 1, pageSize = 10 }) {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `${
+          import.meta.env.VITE_PUBLIC_BACKEND_URL
+        }/blog/getBlogs?page=${page}&pageSize=${pageSize}`
+      );
+      setBlogs(data?.blogs);
+      setTotalBlogs(data?.totalDocuments || 0);
+      setPageSize(data?.pageSize || 10);
+      setPage(data?.currentPage || 1);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    getBlogs();
-  }, []);
+  useEffect(() => {
+    fetchBlog({ page: page, pageSize: pageSize });
+  }, [page, pageSize]);
 
   if (loading) {
     return (
@@ -74,6 +87,46 @@ function BlogPage() {
         ) : (
           <div>No blogs available</div>
         )}
+      </div>
+      <div className="mb-20 flex w-full items-center justify-center  gap-2">
+        <div className="">
+          <Button
+            variant="ghost"
+            onClick={() => setPage(page - 1)}
+            disabled={page === 1}
+          >
+            Previous
+          </Button>
+          <Button>{page}</Button>
+          <Button
+            variant="ghost"
+            onClick={() => setPage(page + 1)}
+            disabled={page === Math.ceil(totalBlogs / pageSize)}
+          >
+            Next
+          </Button>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              {pageSize}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuItem onClick={() => setPageSize(10)}>
+              10
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPageSize(20)}>
+              20
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPageSize(50)}>
+              50
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPageSize(100)}>
+              100
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </>
   );
