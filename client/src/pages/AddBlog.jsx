2a45415168a263c1b2799771d34720a8434e4d63
Shubham priya <141516634+shubhampriya1@users.noreply.github.com>
2024-08-03 16:46:01 +0530

    Update AddBlog.jsx

--- client/src/pages/AddBlog.jsx
+++ client/src/pages/AddBlog.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-
-import { Textarea } from "@/components/ui/textarea";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { getUserData } from "@/lib/user";
+import { Textarea } from "@/components/ui/textarea";
+import Navbar from "@/components/navbar";
 
 const AddBlog = () => {
+  const query = new URLSearchParams(useLocation().search);
+  const blogId = query.get("blogId");
+
   const [title, setTitle] = useState("");
   const [slug, setSlug] = useState("");
   const [img, setImg] = useState("");
   const [content, setContent] = useState("");
-
   const [loading, setloading] = useState(false);
 
   const navigate = useNavigate();
@@ -28,6 +29,29 @@ const AddBlog = () => {
     }
   }, [userid, navigate]);
 
+  useEffect(() => {
+    if (blogId) {
+      setloading(true);
+      axios
+        .get(
+          `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/blog/getBlog/${blogId}`
+        )
+        .then((res) => {
+          const { title, slug, img, content } = res.data;
+          setTitle(title);
+          setSlug(slug);
+          setImg(img);
+          setContent(content);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          setloading(false);
+        });
+    }
+  }, [blogId]);
+
   const disabled =
     title.length < 10 ||
     slug.length < 10 ||
@@ -36,9 +60,8 @@ const AddBlog = () => {
 
   async function onSubmit() {
     try {
-      console.log(userid);
       setloading(true);
-      const response = await axios.post(
+      await axios.post(
         `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/blog/createBlog`,
         {
           title,
@@ -64,77 +87,115 @@ const AddBlog = () => {
     }
   }
 
+  async function updateBlog() {
+    try {
+      setloading(true);
+      await axios.post(
+        `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/blog/editblog`,
+        {
+          title,
+          img,
+          slug,
+          content: content,
+          id: blogId,
+          username: userid,
+        }
+      );
+
+      toast.success("Updated your blog");
+      setContent("");
+      setImg("");
+      setTitle("");
+      setSlug("");
+
+      navigate(`/blogs`);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data);
+    } finally {
+      setloading(false);
+    }
+  }
+
   return (
-    <div className="max-w-screen-xl md:mx-10 sm:mx-10 space-y-5 mx-3">
-      <h1 className="text-2xl text-center my-6">Add your blog</h1>
-      <div className="grid w-full items-center gap-4">
-        <div className="flex flex-col items-start gap-1 space-y-1.5">
-          <Label htmlFor="title">Title</Label>
-          <Input
-            id="title"
-            placeholder="Enter your Title"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <p className="text-sm text-muted-foreground">
-            Title must be more than 10 characters
-          </p>
+    <>
+      <Navbar />
+      <div className="max-w-screen-xl md:mx-10 sm:mx-10 space-y-5 mx-3 my-20">
+        <h1 className="text-2xl text-center my-6">Write your blog</h1>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col items-start gap-1 space-y-1.5">
+            <Label htmlFor="title">Title</Label>
+            <Input
+              id="title"
+              placeholder="Enter your Title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <p className="text-sm text-muted-foreground">
+              Title must be more than 10 characters
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="grid w-full items-center gap-4">
-        <div className="flex flex-col items-start gap-1 space-y-1.5">
-          <Label htmlFor="img">Image Url</Label>
-          <Input
-            id="img"
-            placeholder="Image url"
-            name="img"
-            value={img}
-            onChange={(e) => setImg(e.target.value)}
-          />
-          <p className="text-sm text-muted-foreground">
-            This is image url of your blog
-          </p>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col items-start gap-1 space-y-1.5">
+            <Label htmlFor="img">Image Url</Label>
+            <Input
+              id="img"
+              placeholder="Image url"
+              name="img"
+              value={img}
+              onChange={(e) => setImg(e.target.value)}
+            />
+            <p className="text-sm text-muted-foreground">
+              This is image url of your blog
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="grid w-full items-center gap-4">
-        <div className="flex flex-col items-start gap-1 space-y-1.5">
-          <Label htmlFor="slug">Slug</Label>
-          <Input
-            id="slug"
-            placeholder="Put your slug here"
-            name="slug"
-            value={slug}
-            onChange={(e) => setSlug(e.target.value)}
-          />
-          <p className="text-sm text-muted-foreground">
-            Slug must be more than 10 characters
-          </p>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col items-start gap-1 space-y-1.5">
+            <Label htmlFor="slug">Slug</Label>
+            <Input
+              id="slug"
+              placeholder="Put your slug here"
+              name="slug"
+              value={slug}
+              onChange={(e) => setSlug(e.target.value)}
+            />
+            <p className="text-sm text-muted-foreground">
+              Slug must be more than 10 characters
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="grid w-full items-center gap-4">
-        <div className="flex flex-col items-start gap-1 space-y-1.5">
-          <Label htmlFor="content">Blog Content</Label>
-          <Textarea
-            id="content"
-            placeholder="Enter your content"
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <p className="text-sm text-muted-foreground">
-            Content must be more than 30 characters
-          </p>
+        <div className="grid w-full items-center gap-4 mb-20">
+          <div className="flex flex-col items-start gap-1 space-y-1.5">
+            <Label htmlFor="content">Blog Content</Label>
+            <Textarea
+              className="w-full h-96"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+
+            <p className="text-xs text-muted-foreground pl-3">
+              *Content must be more than 30 characters
+            </p>
+          </div>
         </div>
+        <Button
+          className="w-full"
+          onClick={() => {
+            if (blogId) {
+              updateBlog();
+            } else {
+              onSubmit();
+            }
+          }}
+          disabled={loading || disabled}
+        >
+          {blogId ? "Update Blog" : "Add Blog"}
+        </Button>
       </div>
-      <Button
-        className="w-full md:w-auto mx-auto"
-        onClick={onSubmit}
-        disabled={loading || disabled}
-      >
-        Add Blog
-      </Button>
-    </div>
+    </>
   );
 };
 
